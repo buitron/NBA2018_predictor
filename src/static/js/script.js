1ae4908c1705e5268dd9ecff45ffb21feee739fd
alex buitron <alex@buitron.com>
2018-04-25 02:54:04 -0700

    html update - alex

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,6 +1,5 @@
 jQuery(function ($) {
     $('button').click(function () {
-        console.log($('#filterDay input:radio:checked').val())
-        console.log($('#filterDay label.active input').val())
+        console.log($(this).val())
     })
 });
