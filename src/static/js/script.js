9699919cba469ad23fa198047bc6412ee56fd07f
alex buitron <alex@buitron.com>
2018-04-25 03:25:41 -0700

    fixed jquery add remove team class

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,5 +1,10 @@
+var previousClass;
+
 jQuery(function ($) {
     $('button').click(function () {
         console.log($(this).val())
+
+        $('#team').removeClass(previousClass).addClass($(this).val())
+        previousClass = $(this).val()
     })
 });
