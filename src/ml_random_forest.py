280331d47c3b07dbb7e4b937cae24b633e6c45c0
alex buitron <alex@buitron.com>
2018-04-28 04:09:07 -0700

    cleaned up the printing json - @lex

--- src/ml_random_forest.py
+++ src/ml_random_forest.py
@@ -14,19 +14,19 @@ y = df_['CHAMPION']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
-rf = RandomForestClassifier(n_estimators=1000)
+rf = RandomForestClassifier(n_estimators=300)
 rf = rf.fit(X_train,y_train)
 
 
-results = {'model_mean_accuracy': rf.score(X_test,y_test)}
-results['feature_importance'] = sorted(zip(rf.feature_importances_,X.columns),reverse=True)
+results = {'model_mean_accuracy': float("{:.4f}".format(rf.score(X_test,y_test)))}
+results['feature_importance'] = sorted(zip([float("{:.4f}".format(feat)) for feat in rf.feature_importances_],X.columns), reverse=True)
 
 complete_2018 = pd.read_csv('static/data/regular_season/2018_nba_stats_rs.csv')
 
 X_2018 = pd.read_csv('static/data/regular_season/2018_nba_stats_rs.csv')
 X_2018.drop(['TEAM', 'GP', 'MIN'], axis=1, inplace=True)
 
-results['team_prediction'] = sorted(zip(complete_2018['TEAM'].tolist(), rf.predict(X_2018).tolist(), [perc[1] for perc in rf.predict_proba(X_2018).tolist()]))
+results['team_prediction'] = sorted(zip([float("{:.4f}".format(perc[1])) for perc in rf.predict_proba(X_2018).tolist()], rf.predict(X_2018).tolist(), complete_2018['TEAM'].tolist()), reverse=True)
 
 # results['prediction'] = rf.predict(X_2018).tolist()
 # results['prediction_probability'] = [perc[1] for perc in rf.predict_proba(X_2018).tolist()]
